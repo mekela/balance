94c8a71dd4e59f96f223c2b64c373e4473ce960b
alex <sania_free@i.ua>
2016-03-19 13:17:46 +0200

    responsive

--- js/common.js
+++ js/common.js
@@ -1,10 +1,16 @@
 $(function(){
+
 	$('.bxslider').bxSlider({
 	auto: true,
 	 auto: ($(".bxslider li").length > 1) ? true: false,
     pager: ($(".bxslider li").length > 1) ? true: false,
 	});
+
 	$('.feedback_slider_wrap ul').bxSlider({});
+
+	$( ".menu_trigger" ).click(function() {
+	  $( ".js-show-menu" ).slideToggle( "slow");
+	});
 });
 
   
